50b285e2f12521b65155b89758db08b398f4570d
Eliseo Velazquez G <eliseovelazquezg@gmail.com>
2024-05-30 19:08:56 -0300

    Agregando clase django 2

---
+++ inicio/views.py
@@ -0,0 +1,76 @@
+from django.shortcuts import render
+from datetime import datetime
+
+from django.http import HttpResponse
+from django.template import Template, Context, loader
+
+import random
+
+def inicio(request):
+    return HttpResponse('Bienvenido a mi INICIO!!')
+
+def template1(request, nombre, apellido, edad):
+    fecha = datetime.now()
+    return HttpResponse(f'<h1>Mi tamplate 1</h1> -- Fecha: {fecha} -- Buenas {nombre} {apellido} {edad}')
+
+def template2(request, nombre, apellido, edad):
+    
+    archivo_abierto = open(r'C:\Users\elise\Desktop\programacion\mi-proyecto\templates\template2.html')
+    # archivo_abierto = open('templates\template2.html')
+    
+    template = Template(archivo_abierto.read())
+    
+    archivo_abierto.close()
+    fecha = datetime.now()
+    
+    datos = {
+        'fecha': fecha,
+        'nombre': nombre,
+        'apellid': apellido,
+        'edad': edad,
+        }
+    
+    contexto = Context(datos)
+    
+    template_renderizado = template.render(contexto)
+    
+    return HttpResponse(template_renderizado)
+
+def template3(request, nombre, apellido, edad):
+    
+    template = loader.get_template('template2.html')
+    
+    fecha = datetime.now()
+    
+    datos = {
+        'fecha': fecha,
+        'nombre': nombre,
+        'apellid': apellido,
+        'edad': edad,
+        }
+    
+
+    template_renderizado = template.render(datos)
+    
+    return HttpResponse(template_renderizado)
+
+def template4(request, nombre, apellido, edad):
+    
+    fecha = datetime.now()
+    
+    datos = {
+        'fecha': fecha,
+        'nombre': nombre,
+        'apellid': apellido,
+        'edad': edad,
+        }
+    
+    return render(request,'template2.html', datos)
+
+def probando(request):
+    
+    lista = list(range(500))
+    
+    numeros = random.choices(lista, k=50)
+    print(numeros)
+    return render(request,'probando_if_for.html', {'numeros': numeros})
