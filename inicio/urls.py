b715a61a94a52f963096981faa4ee71b94b41f64
Eliseo Velazquez G <eliseovelazquezg@gmail.com>
2024-06-04 21:10:37 -0300

    Clase 20

--- inicio/urls.py
+++ inicio/urls.py
@@ -2,11 +2,11 @@ from django.urls import path
 from inicio import views
 
 urlpatterns = [
-    path('', views.inicio),
+    path('', views.inicio, name="inicio"),
     path('template1/<str:nombre>/<str:apellido>/<int:edad>/', views.template1),
     path('template2/<str:nombre>/<str:apellido>/<int:edad>/', views.template2),
     path('template3/<str:nombre>/<str:apellido>/<int:edad>/', views.template3),
-    path('template4/<str:nombre>/<str:apellido>/<int:edad>/', views.template4),
-    path('probando/', views.probando),
-    path('auto/<str:marca>/<str:modelo>', views.auto),
+    path('saludo/<str:nombre>/<str:apellido>/<int:edad>/', views.template4,name='saludo'),
+    path('prueba/', views.probando,name='probando'),
+    path('auto/<str:marca>/<str:modelo>', views.auto, name='crear'),
 ]
